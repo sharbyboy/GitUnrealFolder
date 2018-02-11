80256e36bb24fd2afbf717d1842433700ba4b38c
WINGZPC\shark <sharkboyjc@gmail.com>
2018-02-11 16:17:29 +1100

    First Commit

---
+++ UdemyEscapeRoom/Source/UdemyEscapeRoom/UdemyEscapeRoomGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UdemyEscapeRoomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMYESCAPEROOM_API AUdemyEscapeRoomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
